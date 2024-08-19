90b4c6dea4ebdb330400747d7fea70f83658bb68
nothing-available <smtkur31@gmail.com>
2024-08-19 22:27:26 +0530

    some bugs fixing

--- client/src/app/dashBoardWrapper.tsx
+++ client/src/app/dashBoardWrapper.tsx
@@ -1,5 +1,33 @@
-const DashBoardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>dashBoardWrapper</div>
+import StoreProvider, { useAppSelector } from "@/redux"
+import NavBar from "./(components)/Navbar"
+import Sidebar from "./(components)/Sidebar"
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  
+  const isSidebarCollapsed = useAppSelector(
+    state => state.global.isSidebarCollapsed
+  )
+
+  return (
+    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}>
+        <NavBar />
+        {children}
+      </main>
+    </div>
+  )
 }
 
-export default DashBoardWrapper
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  )
+}
+
+export default DashboardWrapper
