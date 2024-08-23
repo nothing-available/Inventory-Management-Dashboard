612eebe576acbfd27439955fc6147fe4ae71343a
nothing-available <smtkur31@gmail.com>
2024-08-23 23:17:33 +0530

    chore: Add .gitignore and package.json for server

--- client/src/app/dashBoardWrapper.tsx
+++ client/src/app/dashBoardWrapper.tsx
@@ -1,15 +1,30 @@
+"use client"
+
 import StoreProvider, { useAppSelector } from "@/redux"
 import NavBar from "./(components)/Navbar"
 import Sidebar from "./(components)/Sidebar"
+import { useEffect } from "react"
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  
   const isSidebarCollapsed = useAppSelector(
     state => state.global.isSidebarCollapsed
   )
 
+  const isDarkMode = useAppSelector(state => state.global.isDarkMode)
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.add("light")
+    }
+  })
+
   return (
-    <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
